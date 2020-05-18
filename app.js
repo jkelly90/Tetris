99aba63bded4d60a22b3da619f0aeb014ecc707b
jkelly90 <jlfkelly@gmail.com>
2020-05-18 15:50:06 +0200

    added undraw function

--- app.js
+++ app.js
@@ -44,14 +44,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
     let currentPosition = 4;
-    let current = theTetrominoes[0][0];
+    let currentRotation = 0;
 
-    // Draw the first rotation in the first tetromino
+    // Randomly select a Tetromino and its first rotation
+    let random = Math.floor(Math.random() * theTetrominoes.length);
+    console.log(random);
+    let current = theTetrominoes[random][currentRotation];
+
+    // Draw the tetromino
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino');
         })
     }
 
-    draw();
+    //  Undraw tettromino
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino');
+        })
+    }
 });
